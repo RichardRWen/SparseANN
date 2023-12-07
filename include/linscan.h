c568e422cec687703158629b9085690052f29253
Richard Wen <rwen1@umd.edu>
2023-12-07 04:38:39 -0500

    refactored inverted_index posting_lists types to be less confusing

--- include/linscan.h
+++ include/linscan.h
@@ -8,27 +8,23 @@
 
 template <typename val_type, typename id_type = uint64_t>
 class inverted_index {
-	public:
-	struct posting_list {
-		struct posted_value {
-			id_type id;
-			val_type value;
+public:
+	struct posted_value {
+		id_type id;
+		val_type value;
 
-			posted_value() : id(0), value(0) {}
-			posted_value(id_type id, val_type value) : id(id), value(value) {}
+		posted_value() : id(0), value(0) {}
+		posted_value(id_type id, val_type value) : id(id), value(value) {}
 
 		static inline bool greater(const posted_value a, const posted_value b) {
 			return a.value > b.value;
 		}
-		};
-
-		std::vector<posted_value> values;
 	};
 
 	unsigned int num_lists;
-	std::vector<posting_list> lists;
+	std::vector<std::vector<posted_value>> posting_lists;
 
-	inverted_index(const unsigned int num_lists) : num_lists(num_lists), lists(num_lists) {}
+	inverted_index(const unsigned int num_lists) : num_lists(num_lists), posting_lists(num_lists) {}
 	inverted_index(const char *filename, const char *filetype, const size_t _num_to_read = -1ULL) {
 		if (strcmp(filetype, "csr") == 0) {
 			std::ifstream indptr_reader(filename);
@@ -47,7 +43,7 @@ class inverted_index {
 			data_reader.seekg((num_vecs + 4) * sizeof(uint64_t) + num_vals * sizeof(unsigned int));
 
 			num_lists = num_dims;
-			lists.resize(num_dims);
+			posting_lists.resize(num_dims);
 
 			size_t num_to_read = (_num_to_read < num_vecs ? _num_to_read : num_vecs);
 
@@ -61,8 +57,7 @@ class inverted_index {
 				for (; indptr_start < indptr_end; indptr_start++) {
 					index_reader.read((char*)(&index), sizeof(unsigned int));
 					data_reader.read((char*)(&data), sizeof(float));
-					typename posting_list::posted_value new_value(i_vecs, data);
-					lists[index].values.push_back(new_value);
+					posting_lists[index].emplace_back(i_vecs, data);
 				}
 			}
 		}
@@ -75,23 +70,22 @@ class inverted_index {
 		int num_coords = 0;
 		for (int i = 0; i < num_lists; i++) {
 			if (vector[i]) {
-				typename posting_list::posted_value new_value(vector_id, vector[i]);
-				lists[i].values.push_back(new_value);
+				posting_lists[i].emplace_back(vector_id, vector[i]);
 				num_coords++;
 			}
 		}
 		return num_coords;
 	}
 
-	std::vector<typename posting_list::posted_value> neighbors(val_type *vector, int k) {
+	std::vector<posted_value> neighbors(val_type *vector, int k) {
 		// Calculate the inner products for all documents that share nonzero coordinates with the query vector
 		std::unordered_map<id_type, val_type> inner_products;
 		std::pair<typename std::unordered_map<id_type, val_type>::iterator, bool> insert_result;
 		for (int i = 0; i < num_lists; i++) {
 			if (vector[i]) {
-				for (int j = 0; j < lists[i].values.size(); j++) {
-					val_type partial_inner_product = lists[i].values[j].value * vector[i];
-					std::pair<id_type, val_type> temp_pair(lists[i].values[j].id, partial_inner_product);
+				for (int j = 0; j < posting_lists[i].size(); j++) {
+					val_type partial_inner_product = posting_lists[i][j].value * vector[i];
+					std::pair<id_type, val_type> temp_pair(posting_lists[i][j].id, partial_inner_product);
 					insert_result = inner_products.insert(temp_pair);
 					if (!insert_result.second) {
 						insert_result.first->second += partial_inner_product;
@@ -101,16 +95,12 @@ class inverted_index {
 		}
 
 		// Use heap to find top k inner products
-		std::vector<typename posting_list::posted_value> heap;
+		std::vector<posted_value> heap;
 		for (auto it = inner_products.begin(); it != inner_products.end(); it++) {
-			typename posting_list::posted_value next_value;
-			next_value.id = it->first;
-			next_value.value = it->second;
-
-			heap.push_back(next_value);
-			std::push_heap(heap.begin(), heap.end(), posting_list::posted_value::greater);
+			heap.emplace_back(it->first, it->second);
+			std::push_heap(heap.begin(), heap.end(), posted_value::greater);
 			if (heap.size() > k) {
-				std::pop_heap(heap.begin(), heap.end(), posting_list::posted_value::greater);
+				std::pop_heap(heap.begin(), heap.end(), posted_value::greater);
 			}
 		}
 
