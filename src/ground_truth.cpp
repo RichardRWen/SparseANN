a4d5f47ee634d63d12333c8d943c642146fd7c96
Richard Wen <rwen1@umd.edu>
2024-02-13 18:07:00 -0500

    minor bug fixes

--- src/ground_truth.cpp
+++ src/ground_truth.cpp
@@ -1,7 +1,7 @@
 #include "ground_truth.h"
 
 template <typename id_type>
-parlay::sequence<parlay::sequence<id_type>> ground_truth(char *inserts_file, char *queries_file, int k = 10) { // Does returning a sequence in this way result in copying overheads?
+parlay::sequence<parlay::sequence<id_type>> ground_truth(char *inserts_file, char *queries_file, int k) { // Does returning a sequence in this way result in copying overheads?
 	// create forward index from file
 	// TODO: assert this is actually a .csr file
 	forward_index<float> fwd_index(inserts_file, "csr");
@@ -12,7 +12,7 @@ parlay::sequence<parlay::sequence<id_type>> ground_truth(char *inserts_file, cha
 	forward_index<float> queries(queries_file, "csr");
 	// call neighbors() on every query in parallel
 	parlay::sequence<parlay::sequence<id_type>> ground_truth(fwd_index.points.size(),
-		[&inv_index, &queries] (size_t i) -> parlay::sequence<id_type> {
+		[&inv_index, &queries, &k] (size_t i) -> parlay::sequence<id_type> {
 			auto neighbors_dist = inv_index.neighbors(queries.points[i], k);
 			parlay::sequence<id_type> neighbors(neighbors_dist.size(), 
 				[&neighbors_dist] (size_t i) -> id_type {
@@ -26,11 +26,11 @@ parlay::sequence<parlay::sequence<id_type>> ground_truth(char *inserts_file, cha
 }
 
 template <typename id_type>
-parlay::sequence<parlay::sequence<id_type>> ground_truth(forward_index<id_type>& inserts, forward_index<id_type>& queries, int k = 10) {
-	inverted_index<float> inv_index(fwd_index);
+parlay::sequence<parlay::sequence<id_type>> ground_truth(forward_index<id_type>& inserts, forward_index<id_type>& queries, int k) {
+	inverted_index<float> inv_index(inserts);
 
-	parlay::sequence<parlay::sequence<id_type>> ground_truth(fwd_index.points.size(),
-		[&inv_index, &queries] (size_t i) -> parlay::sequence<id_type> {
+	parlay::sequence<parlay::sequence<id_type>> ground_truth(queries.points.size(),
+		[&inv_index, &queries, &k] (size_t i) -> parlay::sequence<id_type> {
 			return inv_index.neighbors(queries.points[i], k);
 		}
 	);
@@ -38,7 +38,7 @@ parlay::sequence<parlay::sequence<id_type>> ground_truth(forward_index<id_type>&
 }
 
 template <typename id_type>
-double recall(parlay::sequence<parlay::sequence<id_type>>& ground_truth, parlay::sequence<parlay::sequence<id_type>>& neighbors, int k = 10) {
+double recall(parlay::sequence<parlay::sequence<id_type>>& ground_truth, parlay::sequence<parlay::sequence<id_type>>& neighbors, int k) {
 	double recall = 0;
 	int i;
 	for (i = 0; i < ground_truth.size() && i < neighbors.size(); i++) {
