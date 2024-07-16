fe72c9896e7c45f8657245ce2a7353c3a38d2b21
Richard Wen <rwen1@umd.edu>
2024-07-16 18:13:27 -0400

    updated transformation evaluation test to be modular with multiple different transformations

--- test/eval_dist_approx.cpp
+++ test/eval_dist_approx.cpp
@@ -54,16 +54,68 @@ double spearman_rank_correlation(const parlay::sequence<float>& original, const 
 }
 
 
+struct test_params {
+    int k;
+    int *overretrievals;
+    int num_overretrievals;
+    int max_overretrieval;
+    int num_evals;
+    int eval_sample;
+};
+
+void evaluate_transformation(
+    std::string transformation_name,
+    forward_index<float>& count_min_inserts,
+    forward_index<float>& count_min_queries,
+    parlay::sequence<parlay::sequence<uint32_t>>& groundtruth,
+    parlay::sequence<parlay::sequence<float>>& distances,
+    test_params &params
+) {
+    parlay::internal::timer timer;
+    timer.start();
+
+    std::cout << "Computing ground truth of " << transformation_name << "...\t" << std::flush;
+    auto count_min_groundtruth = ground_truth(count_min_inserts, count_min_queries, params.max_overretrieval);
+    std::cout << "Done in " << timer.next_time() << " seconds" << std::endl;
+    for (int i = 0; i < params.num_overretrievals; i++) {
+        double recall = get_recall(groundtruth, count_min_groundtruth, params.k, params.overretrievals[i]);
+        std::cout << "Recall " << params.k << "@" << params.overretrievals[i] << ":\t" << recall << std::endl;
+    }
+    std::cout << std::endl;
+
+    double avg_spearman_rank = 0, min_spearman_rank = 1, max_spearman_rank = -1;
+    for (int i = 0; i < params.num_evals; i++) {
+        std::cout << "\rComputing Spearman's rank correlation coefficients...\t" << i << "/" << params.num_evals << std::flush;
+
+        auto count_min_distances = parlay::sequence<float>::from_function(params.eval_sample, [&] (size_t j) {
+            return forward_index<float>::dist(count_min_queries.points[i], count_min_inserts.points[j]);
+        });
+
+        double spearman_rank = spearman_rank_correlation(distances[i], count_min_distances);
+        avg_spearman_rank += spearman_rank;
+        if (spearman_rank < min_spearman_rank) min_spearman_rank = spearman_rank;
+        if (spearman_rank > max_spearman_rank) max_spearman_rank = spearman_rank;
+    }
+    avg_spearman_rank /= params.num_evals;
+    std::cout << "\rComputing Spearman's rank correlation coefficients...\t" << params.num_evals << "/" << params.num_evals << " in " << timer.next_time() << " seconds" << std::endl;
+    std::cout << "Min:\t" << min_spearman_rank << std::endl;
+    std::cout << "Avg:\t" << avg_spearman_rank << std::endl;
+    std::cout << "Max:\t" << max_spearman_rank << std::endl << std::endl;
+}
+
+
 int main(int argc, char **argv) {
-	int k = 10;
+    test_params params;
+    params.k = 10;
 	int overretrievals[7] = {1, 2, 5, 10, 20, 50, 100};
-    int num_overretrievals = sizeof(overretrievals) / sizeof(overretrievals[0]);
-	for (int i = 0; i < num_overretrievals; i++) {
-		overretrievals[i] *= k;
+    params.num_overretrievals = sizeof(overretrievals) / sizeof(overretrievals[0]);
+	for (int i = 0; i < params.num_overretrievals; i++) {
+		overretrievals[i] *= params.k;
 	}
-    int max_overretrieval = overretrievals[num_overretrievals - 1];
-    int num_evals = 100;
-    int eval_sample = 1000;
+    params.overretrievals = overretrievals;
+    params.max_overretrieval = overretrievals[params.num_overretrievals - 1];
+    params.num_evals = 100;
+    params.eval_sample = 1000;
 
     srand(time(NULL));
     parlay::internal::timer timer;
@@ -76,12 +128,12 @@ int main(int argc, char **argv) {
     std::cout << "Done in " << timer.next_time() << " seconds" << std::endl;
 
     std::cout << "Computing ground truth of inputs...\t" << std::flush;
-    auto gt = ground_truth(inserts, queries, k);
+    auto groundtruth = ground_truth(inserts, queries, params.k);
     std::cout << "Done in " << timer.next_time() << " seconds" << std::endl;
 
     std::cout << "Computing sample distances...\t" << std::flush;
-    auto distances = parlay::sequence<parlay::sequence<float>>::from_function(num_evals, [&] (size_t i) {
-        return  parlay::sequence<float>::from_function(eval_sample, [&] (size_t j) {
+    auto distances = parlay::sequence<parlay::sequence<float>>::from_function(params.num_evals, [&] (size_t i) {
+        return parlay::sequence<float>::from_function(params.eval_sample, [&] (size_t j) {
             return forward_index<float>::dist(queries.points[i], inserts.points[j]);
         });
     });
@@ -91,8 +143,8 @@ int main(int argc, char **argv) {
     size_t count_min_quant_dims = 200;
     std::cout << "Generating count min sketch of dimension " << count_min_quant_dims << "...\t" << std::flush;
     auto count_min_sketch_200 = count_min_sketch(inserts.dims, count_min_quant_dims);
-    auto count_min_inserts = forward_index<float>(quant_dims);
-    auto count_min_queries = forward_index<float>(quant_dims);
+    auto count_min_inserts = forward_index<float>(count_min_quant_dims);
+    auto count_min_queries = forward_index<float>(count_min_quant_dims);
     count_min_inserts.points = parlay::sequence<parlay::sequence<std::pair<uint32_t, float>>>::from_function(inserts.size(), [&] (size_t i) {
         auto qvec = count_min_sketch_200.transform_csr_to_qvec(inserts.points[i]);
         return count_min_sketch_200.transform_qvec_to_qcsr(qvec);
@@ -102,8 +154,10 @@ int main(int argc, char **argv) {
         return count_min_sketch_200.transform_qvec_to_qcsr(qvec);
     });
     std::cout << "Done in " << timer.next_time() << " seconds" << std::endl << std::endl;
+
+    evaluate_transformation("count min sketch", count_min_inserts, count_min_queries, groundtruth, distances, params);
     
-    std::cout << "Computing ground truth of count min sketch...\t" << std::flush;
+    /*std::cout << "Computing ground truth of count min sketch...\t" << std::flush;
     auto count_min_gt = ground_truth(count_min_inserts, count_min_queries, max_overretrieval);
     std::cout << "Done in " << timer.next_time() << " seconds" << std::endl;
     for (int i = 0; i < num_overretrievals; i++) {
@@ -116,14 +170,11 @@ int main(int argc, char **argv) {
     for (int i = 0; i < num_evals; i++) {
         std::cout << "\rComputing Spearman's rank correlation coefficients...\t" << i << "/" << num_evals << std::flush;
 
-        auto distances = parlay::sequence<float>::from_function(eval_sample, [&] (size_t j) {
-            return forward_index<float>::dist(queries.points[i], inserts.points[j]);
-        });
         auto count_min_distances = parlay::sequence<float>::from_function(eval_sample, [&] (size_t j) {
             return forward_index<float>::dist(count_min_queries.points[i], count_min_inserts.points[j]);
         });
 
-        double spearman_rank = spearman_rank_correlation(distances, count_min_distances);
+        double spearman_rank = spearman_rank_correlation(distances[i], count_min_distances);
         avg_spearman_rank += spearman_rank;
         if (spearman_rank < min_spearman_rank) min_spearman_rank = spearman_rank;
         if (spearman_rank > max_spearman_rank) max_spearman_rank = spearman_rank;
@@ -132,52 +183,5 @@ int main(int argc, char **argv) {
     std::cout << "\rComputing Spearman's rank correlation coefficients...\t" << num_evals << "/" << num_evals << " in " << timer.next_time() << " seconds" << std::endl;
     std::cout << "Min:\t" << min_spearman_rank << std::endl;
     std::cout << "Avg:\t" << avg_spearman_rank << std::endl;
-    std::cout << "Max:\t" << max_spearman_rank << std::endl << std::endl;
-
-    // SINNAMON
-    size_t quant_dims = 200;
-    std::cout << "Generating Sinnamon sketch of dimension " << quant_dims << "...\t" << std::flush;
-    auto count_min_sketch_200 = count_min_sketch(inserts.dims, quant_dims);
-    auto count_min_inserts = forward_index<float>(quant_dims);
-    auto count_min_queries = forward_index<float>(quant_dims);
-    count_min_inserts.points = parlay::sequence<parlay::sequence<std::pair<uint32_t, float>>>::from_function(inserts.size(), [&] (size_t i) {
-        auto qvec = count_min_sketch_200.transform_csr_to_qvec(inserts.points[i]);
-        return count_min_sketch_200.transform_qvec_to_qcsr(qvec);
-    });
-    count_min_queries.points = parlay::sequence<parlay::sequence<std::pair<uint32_t, float>>>::from_function(queries.size(), [&] (size_t i) {
-        auto qvec = count_min_sketch_200.transform_csr_to_qvec(queries.points[i]);
-        return count_min_sketch_200.transform_qvec_to_qcsr(qvec);
-    });
-    std::cout << "Done in " << timer.next_time() << " seconds" << std::endl << std::endl;
-    
-    std::cout << "Computing ground truth of Sinnamon sketch...\t" << std::flush;
-    auto count_min_gt = ground_truth(count_min_inserts, count_min_queries, max_overretrieval);
-    std::cout << "Done in " << timer.next_time() << " seconds" << std::endl;
-    for (int i = 0; i < num_overretrievals; i++) {
-        double recall = get_recall(gt, count_min_gt, k, overretrievals[i]);
-        std::cout << "Recall " << k << "@" << overretrievals[i] << ":\t" << recall << std::endl;
-    }
-    std::cout << std::endl;
-
-    double avg_spearman_rank = 0, min_spearman_rank = 1, max_spearman_rank = -1;
-    for (int i = 0; i < num_evals; i++) {
-        std::cout << "\rComputing Spearman's rank correlation coefficients...\t" << i << "/" << num_evals << std::flush;
-
-        auto distances = parlay::sequence<float>::from_function(eval_sample, [&] (size_t j) {
-            return forward_index<float>::dist(queries.points[i], inserts.points[j]);
-        });
-        auto count_min_distances = parlay::sequence<float>::from_function(eval_sample, [&] (size_t j) {
-            return forward_index<float>::dist(count_min_queries.points[i], count_min_inserts.points[j]);
-        });
-
-        double spearman_rank = spearman_rank_correlation(distances, count_min_distances);
-        avg_spearman_rank += spearman_rank;
-        if (spearman_rank < min_spearman_rank) min_spearman_rank = spearman_rank;
-        if (spearman_rank > max_spearman_rank) max_spearman_rank = spearman_rank;
-    }
-    avg_spearman_rank /= num_evals;
-    std::cout << "\rComputing Spearman's rank correlation coefficients...\t" << num_evals << "/" << num_evals << " in " << timer.next_time() << " seconds" << std::endl;
-    std::cout << "Min:\t" << min_spearman_rank << std::endl;
-    std::cout << "Avg:\t" << avg_spearman_rank << std::endl;
-    std::cout << "Max:\t" << max_spearman_rank << std::endl << std::endl;
+    std::cout << "Max:\t" << max_spearman_rank << std::endl << std::endl;*/
 }
