4c85a5ad6bb9722521cd8dc9ec61a0502ac0877b
Richard Wen <rwen1@umd.edu>
2024-09-12 10:49:59 -0400

    added conversions from full precision to quantized vectors

--- benchmark/parlayann_unit_test.cpp
+++ benchmark/parlayann_unit_test.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cassert>
 
 #include <parlay/parallel.h>
 #include <parlay/primitives.h>
@@ -16,6 +17,7 @@
 
 #include "forward_index.h"
 #include "point_range_util.h"
+#include "vector_utils.h"
 
 #include "sinnamon_sketch.h"
 #include "count_min_sketch.h"
@@ -27,14 +29,14 @@ using point_type = SparseMipsPoint<value_type>;
 using point_range_type = SparsePointRange<value_type, point_type>;
 
 int main(int argc, char* argv[]) {
-    std::string query_type = ""; // default query type
+    std::string quant_type = ""; // default query type
     if (argc > 1) {
         std::string arg = argv[1];
         if (arg == "-q") {
             if (argc > 2) {
                 std::string query_arg = argv[2];
                 if (query_arg == "countmin" || query_arg == "sinnamon" || query_arg == "jl" || query_arg == "rabitq") {
-                    query_type = query_arg;
+                    quant_type = query_arg;
                 } else {
                     std::cout << "Invalid query type" << std::endl;
                     return 1;
@@ -66,12 +68,18 @@ int main(int argc, char* argv[]) {
     );
 
     char pr_file[] = "data/base_small.csr";
-    auto PR_fi = forward_index<value_type>(pr_file, "csr");
-    auto PR = fi_to_spr<value_type, point_type>(PR_fi);
+    auto inserts = forward_index<value_type>(pr_file, "csr");
 
     char q_file[] = "data/queries.dev.csr";
-    auto Q_fi = forward_index<value_type>(q_file, "csr");
-    auto Q = fi_to_spr<value_type, point_type>(Q_fi);
+    auto queries = forward_index<value_type>(q_file, "csr");
+
+    if (inserts.dims != queries.dims) {
+        std::cout << "Dimensionality of inserts and queries must match: " << inserts.dims << " != " << queries.dims << std::endl;
+        return 1;
+    }
+
+    auto PR = fi_to_spr<value_type, point_type>(inserts);
+    auto Q = fi_to_spr<value_type, point_type>(queries);
 
     char gt_file[] = "data/base_small.dev.gt";
     auto GT = groundTruth<index_type>(gt_file);
@@ -81,21 +89,56 @@ int main(int argc, char* argv[]) {
 
     char r_file[] = "res_output.txt";
 
-    point_range_type QQ;
     point_range_type QPR;
-    if (query_type == "countmin") {
-        QQ = Q;
-        QPR = PR;
+    point_range_type QQ;
+    if (quant_type == "countmin") {
+        auto sketch = count_min_sketch(inserts.dims, 400);
+        forward_index<value_type> qinserts, qqueries;
+        qinserts.dims = qqueries.dims = 400;
+        qinserts.points = parlay::tabulate(inserts.size(), [&] (size_t i) {
+            auto qvec = sketch.transform_csr_to_qvec<value_type>(inserts[i]);
+            return sketch.transform_qvec_to_qcsr<value_type>(qvec);
+        });
+        qqueries.points = parlay::tabulate(queries.size(), [&] (size_t i) {
+            auto qvec = sketch.transform_csr_to_qvec<value_type>(queries[i]);
+            return sketch.transform_qvec_to_qcsr<value_type>(qvec);
+        });
+        QPR = fi_to_spr<value_type, point_type>(qinserts);
+        QQ = fi_to_spr<value_type, point_type>(qqueries);
     }
-    else if (query_type == "sinnamon") {
-        QQ = Q;
-        QPR = PR;
+    else if (quant_type == "sinnamon") {
+        auto sketch = sinnamon_sketch(inserts.dims, 400);
+        forward_index<value_type> qinserts, qqueries;
+        qinserts.dims = qqueries.dims = 400;
+        qinserts.points = parlay::tabulate(inserts.size(), [&] (size_t i) {
+            auto qvec = sketch.transform_csr_to_qvec<value_type>(inserts[i]);
+            return sketch.transform_qvec_to_qcsr<value_type>(qvec);
+        });
+        qqueries.points = parlay::tabulate(queries.size(), [&] (size_t i) {
+            auto qvec = sketch.transform_csr_to_qvec<value_type>(queries[i]);
+            return sketch.transform_qvec_to_qcsr<value_type>(qvec);
+        });
+        QPR = fi_to_spr<value_type, point_type>(qinserts);
+        QQ = fi_to_spr<value_type, point_type>(qqueries);
     }
-    else if (query_type == "jl") {
-        QQ = Q;
-        QPR = PR;
+    else if (quant_type == "jl") {
+        auto projection_matrix = generate_random_projection_matrix(inserts.dims, 400);
+        forward_index<value_type> qinserts, qqueries;
+        qinserts.dims = qqueries.dims = 400;
+        qinserts.points = parlay::tabulate(queries.size(), [&] (size_t i) {
+            auto insert_vec = csr_to_vec(queries[i], queries.dims);
+            auto jl_transform = apply_jl_transform(insert_vec, projection_matrix);
+            return vec_to_csr(jl_transform);
+        });
+        qqueries.points = parlay::tabulate(inserts.size(), [&] (size_t i) {
+            auto insert_vec = csr_to_vec(inserts[i], inserts.dims);
+            auto jl_transform = apply_jl_transform(insert_vec, projection_matrix);
+            return vec_to_csr(jl_transform);
+        });
+        QPR = fi_to_spr<value_type, point_type>(qinserts);
+        QQ = fi_to_spr<value_type, point_type>(qqueries);
     }
-    else if (query_type == "rabitq") {
+    else if (quant_type == "rabitq") {
         QQ = Q;
         QPR = PR;
     }
