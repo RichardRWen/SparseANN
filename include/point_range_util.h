bdfcd339529c8b502f1baec70ce12313405ffc40
Richard Wen <rwen1@umd.edu>
2024-09-12 10:01:57 -0400

    fixed indptr concurrency issues in forward_index to SparsePointRange conversion

--- include/point_range_util.h
+++ include/point_range_util.h
@@ -12,7 +12,7 @@
 
 
 template<typename T, class Point_>
-SparsePointRange<T, Point_> fi_to_spr(forward_index<T> &fwd_index) {
+SparsePointRange<T, Point_> fi_to_spr(const forward_index<T> &fwd_index) {
     SparsePointRange<T, Point_> PR;
 
     PR.n = fwd_index.size();
@@ -21,18 +21,19 @@ SparsePointRange<T, Point_> fi_to_spr(forward_index<T> &fwd_index) {
     auto vector_lens = parlay::delayed_tabulate(PR.n, [&] (size_t i) {
         return fwd_index[i].size();
     });
-    size_t num_values = parlay::reduce(vector_lens);
+    auto [prefix_sums, num_values] = parlay::scan(vector_lens);
 
     PR.indptrs = (uint64_t*) malloc((PR.n + 1) * sizeof(uint64_t));
     PR.indices = (unsigned int*) malloc(num_values * sizeof(unsigned int));
     PR.values = (T*) malloc(num_values * sizeof(T));
     parlay::parallel_for(0, PR.n, [&] (size_t i) {
-        PR.indptrs[i] = i == 0 ? 0 : PR.indptrs[i - 1] + fwd_index[i - 1].size();
+        PR.indptrs[i] = prefix_sums[i];
         for (size_t j = 0; j < fwd_index[i].size(); j++) {
             PR.indices[PR.indptrs[i] + j] = fwd_index[i][j].first;
             PR.values[PR.indptrs[i] + j] = fwd_index[i][j].second;
         }
     });
+    PR.indptrs[PR.n] = num_values;
 
     return PR;
 }
